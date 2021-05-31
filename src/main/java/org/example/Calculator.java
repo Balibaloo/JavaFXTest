185d9287d0b136f0ba5124f5334672dc2835e9ac
Roman Kubiv <romankubiv101@gmail.com>
2021-05-31 01:27:10 +0100

    Add Calculator Display interface

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,10 +3,31 @@ package org.example;
 import java.util.ArrayList;
 
 public class Calculator {
+  private Display display;
 
   private ArrayList<Character> argumentStorage;
 
   public Calculator() {
     argumentStorage = new ArrayList<>();
+  }
+  
+  public Calculator(Display d) {
+    this();
+    display = d;
+  }
+
+  // used to update data when the argument storage changes
+  public interface Display {
+    void show(String data);
+  }
+
+  public void setDisplay(Display display) {
+    this.display = display;
+  }
+
+  private void ifDisplayThenShow(String data) {
+    if (display != null) {
+      display.show(data);
+    }
   }
 }
