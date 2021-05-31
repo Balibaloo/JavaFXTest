82e6338566b5ff7fd2837eae5646a59fef4849f7
Roman Kubiv <romankubiv101@gmail.com>
2021-05-31 01:46:09 +0100

    Add app window title

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,7 +15,7 @@ public class App extends Application {
     @Override
     public void start(@SuppressWarnings("exports") Stage s) throws IOException {
         stage = s;
-        setRoot("primary");
+        setRoot("primary", "Calculator");
     }
 
     static void setRoot(String fxml) throws IOException {
