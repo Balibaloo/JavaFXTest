a25cbd3f735b8584e2fef4fb81278f6bbf2903c1
Roman Kubiv <romankubiv101@gmail.com>
2021-05-31 01:12:37 +0100

    Add Calculator argumentStorage
    Add array to store arguments as characters

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,7 +1,12 @@
 package org.example;
 
-public class Calculator {
-  public Calculator() {
+import java.util.ArrayList;
 
+public class Calculator {
+
+  private ArrayList<Character> argumentStorage;
+
+  public Calculator() {
+    argumentStorage = new ArrayList<>();
   }
 }
